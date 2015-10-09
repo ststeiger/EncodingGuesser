c2427641467056fefd8db6a44e992255f859b770
ststeiger <quandary82@hailmail.net>
2015-10-09 14:23:31 +0200

    Initial commit

---
+++ EncodingTester/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace EncodingTester
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvResult = new System.Windows.Forms.DataGridView();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnTestAll = new System.Windows.Forms.Button();
+            this.txtCorrect = new System.Windows.Forms.TextBox();
+            this.lblCorrect = new System.Windows.Forms.Label();
+            this.txtWrong = new System.Windows.Forms.TextBox();
+            this.lblWrong = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvResult
+            // 
+            this.dgvResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dgvResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResult.Location = new System.Drawing.Point(12, 93);
+            this.dgvResult.Name = "dgvResult";
+            this.dgvResult.Size = new System.Drawing.Size(923, 475);
+            this.dgvResult.TabIndex = 0;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 587);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(91, 23);
+            this.btnTest.TabIndex = 1;
+            this.btnTest.Text = "Test Common";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnTestAll
+            // 
+            this.btnTestAll.Location = new System.Drawing.Point(109, 587);
+            this.btnTestAll.Name = "btnTestAll";
+            this.btnTestAll.Size = new System.Drawing.Size(75, 23);
+            this.btnTestAll.TabIndex = 2;
+            this.btnTestAll.Text = "Test All";
+            this.btnTestAll.UseVisualStyleBackColor = true;
+            this.btnTestAll.Click += new System.EventHandler(this.btnTestAll_Click);
+            // 
+            // txtCorrect
+            // 
+            this.txtCorrect.Location = new System.Drawing.Point(86, 29);
+            this.txtCorrect.Name = "txtCorrect";
+            this.txtCorrect.Size = new System.Drawing.Size(849, 20);
+            this.txtCorrect.TabIndex = 3;
+            // 
+            // lblCorrect
+            // 
+            this.lblCorrect.AutoSize = true;
+            this.lblCorrect.Location = new System.Drawing.Point(12, 32);
+            this.lblCorrect.Name = "lblCorrect";
+            this.lblCorrect.Size = new System.Drawing.Size(68, 13);
+            this.lblCorrect.TabIndex = 4;
+            this.lblCorrect.Text = "Correct Text:";
+            // 
+            // txtWrong
+            // 
+            this.txtWrong.Location = new System.Drawing.Point(86, 55);
+            this.txtWrong.Name = "txtWrong";
+            this.txtWrong.Size = new System.Drawing.Size(849, 20);
+            this.txtWrong.TabIndex = 5;
+            // 
+            // lblWrong
+            // 
+            this.lblWrong.AutoSize = true;
+            this.lblWrong.Location = new System.Drawing.Point(12, 58);
+            this.lblWrong.Name = "lblWrong";
+            this.lblWrong.Size = new System.Drawing.Size(66, 13);
+            this.lblWrong.TabIndex = 6;
+            this.lblWrong.Text = "Wrong Text:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 628);
+            this.Controls.Add(this.lblWrong);
+            this.Controls.Add(this.txtWrong);
+            this.Controls.Add(this.lblCorrect);
+            this.Controls.Add(this.txtCorrect);
+            this.Controls.Add(this.btnTestAll);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.dgvResult);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encoding Guesser";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvResult;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnTestAll;
+        private System.Windows.Forms.TextBox txtCorrect;
+        private System.Windows.Forms.Label lblCorrect;
+        private System.Windows.Forms.TextBox txtWrong;
+        private System.Windows.Forms.Label lblWrong;
+    }
+}
+
---
+++ EncodingTester/Form1.cs
@@ -0,0 +1,164 @@
+﻿
+using System.Windows.Forms;
+
+
+namespace EncodingTester
+{
+
+
+    public partial class Form1 : Form
+    {
+        public System.Data.DataTable dt;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public System.Data.DataTable CreateTable()
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            dt.Columns.Add("SourceEncoding", typeof(string));
+            dt.Columns.Add("TargetEncoding", typeof(string));
+            dt.Columns.Add("Result", typeof(string));
+            dt.Columns.Add("SourceEncodingName", typeof(string));
+            dt.Columns.Add("TargetEncodingName", typeof(string));
+
+            return dt;
+        }
+
+
+        private void btnTest_Click(object sender, System.EventArgs e)
+        {
+            dt = CreateTable();
+
+            string correct = "Brokers México, Intermediario de Aseguro,S.A.";
+            string broken = "Brokers MÃ©xico, Intermediario de Aseguro,S.A."; // Get text from database
+
+            correct = this.txtCorrect.Text;
+            broken = this.txtWrong.Text;
+
+
+            // For reference
+            // https://msdn.microsoft.com/en-us/library/system.text.encodinginfo.getencoding(v=vs.110).aspx
+            int[] encs = new int[] { 
+                 20127 // US-ASCII
+                ,28591 // iso-8859-1 Western European (ISO)       
+                ,28592 // iso-8859-2 Central European (ISO)       
+                ,28593 // iso-8859-3 Latin 3 (ISO)
+                ,28594 // iso-8859-4 Baltic (ISO)
+                ,28595 // iso-8859-5 Cyrillic (ISO)
+                ,28596 // iso-8859-6 Arabic (ISO)
+                ,28597 // iso-8859-7 Greek (ISO)
+                ,28598 // iso-8859-8 Hebrew (ISO-Visual)          
+                ,28599 // iso-8859-9 Turkish (ISO)
+                ,28603 // iso-8859-13 Estonian (ISO)
+                ,28605 // iso-8859-15 Latin 9 (ISO)   
+     
+                ,1250 // windows-1250 Central European (Windows)      
+                ,1251 // windows-1251 Cyrillic (Windows)             
+                ,1252 // Windows-1252 Western European (Windows)      
+                ,1253 // windows-1253 Greek (Windows)                
+                ,1254 // windows-1254 Turkish (Windows)              
+                ,1255 // windows-1255 Hebrew (Windows)               
+                ,1256 // windows-1256 Arabic (Windows)               
+                ,1257 // windows-1257 Baltic (Windows)               
+                ,1258 // windows-1258 Vietnamese (Windows)
+
+                ,20866 // Cyrillic (KOI8-R)
+                ,21866 // Cyrillic (KOI8-U)  
+
+                ,65000 // UTF-7
+                ,65001 // UTF-8
+                ,1200 // UTF-16
+                ,1201 // Unicode (Big-Endian)    
+
+                ,12000 // UTF-32
+                ,12001 // UTF-32BE (UTF-32 Big-Endian) 
+            };
+
+
+            for (int i = 0; i < encs.Length; ++i)
+            {
+
+                for (int j = 0; j < encs.Length; ++j)
+                {
+                    System.Data.DataRow dr = dt.NewRow();
+
+                    dr["SourceEncoding"] = encs[i];
+                    dr["TargetEncoding"] = encs[j];
+
+
+                    System.Text.Encoding enci = System.Text.Encoding.GetEncoding(encs[i]);
+                    System.Text.Encoding encj = System.Text.Encoding.GetEncoding(encs[j]);
+
+                    byte[] encoded = enci.GetBytes(broken);
+                    string corrected = encj.GetString(encoded);
+
+                    dr["Result"] = corrected;
+
+                    dr["SourceEncodingName"] = enci.BodyName;
+                    dr["TargetEncodingName"] = encj.BodyName;
+
+
+                    if (System.StringComparer.InvariantCultureIgnoreCase.Equals(correct, corrected))
+                        dt.Rows.Add(dr);
+                } // Next j 
+
+            } // Next i 
+
+            this.dgvResult.DataSource = dt;
+        }
+
+
+        private void btnTestAll_Click(object sender, System.EventArgs e)
+        {
+            dt = CreateTable();
+
+            string correct = "Brokers México, Intermediario de Aseguro,S.A.";
+            string broken = "Brokers MÃ©xico, Intermediario de Aseguro,S.A."; // Get text from database
+
+            correct = this.txtCorrect.Text;
+            broken = this.txtWrong.Text;
+
+
+            System.Text.EncodingInfo[] encs = System.Text.Encoding.GetEncodings();
+            for (int i = 0; i < encs.Length; ++i)
+            {
+
+                for (int j = 0; j < encs.Length; ++j)
+                {
+                    System.Data.DataRow dr = dt.NewRow();
+
+                    dr["SourceEncoding"] = encs[i].CodePage;
+                    dr["TargetEncoding"] = encs[j].CodePage;
+
+
+                    System.Text.Encoding enci = System.Text.Encoding.GetEncoding(encs[i].CodePage);
+                    System.Text.Encoding encj = System.Text.Encoding.GetEncoding(encs[j].CodePage);
+
+                    byte[] encoded = enci.GetBytes(broken);
+                    string corrected = encj.GetString(encoded);
+
+                    dr["Result"] = corrected;
+
+                    dr["SourceEncodingName"] = enci.BodyName;
+                    dr["TargetEncodingName"] = encj.BodyName;
+
+
+                    if (System.StringComparer.InvariantCultureIgnoreCase.Equals(correct, corrected))
+                        dt.Rows.Add(dr);
+                } // Next j 
+
+            } // Next i 
+
+            this.dgvResult.DataSource = dt;
+        } // End Function btnTestAll_Click
+
+
+    } // End Class 
+
+
+} // End Namespace 
